bd4bb90e2c793b06867928f661d20dcdc11397a9
Atish Nishant <nishant.dev2023@gmail.com>
2024-11-17 19:26:39 +0530

    checkpoint

--- src/modules/components/layout/header/index.jsx
+++ src/modules/components/layout/header/index.jsx
@@ -2,14 +2,39 @@ import { faUser } from "@fortawesome/free-regular-svg-icons";
 import { faGear } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
+import noteMock from '../../../../sample-mock/task-mock.json';
+
 function AppHeader() {
     const [showDropDownForSetting,setShowDropDownForSetting]=useState(false);
+    const [suggestedNotesArray,setSuggestedNotesArray]=useState([]);
+    const {notes}=noteMock;
+    const SIZE=5;
+    function handleOnSearch(val){
+        if(val){
+            const tempArray=notes?.filter((note)=>note?.title?.toLowerCase()?.startsWith(val?.toLowerCase()));
+            setSuggestedNotesArray([...tempArray]);
+        }
+        else{
+            setSuggestedNotesArray([]);
+        }
+    }
     return (
     <div className="p-1">
         <div className="bg-secondary rounded-lg p-4">
             <div className="flex justify-between items-center">
                 <div className="flex-[0.8]">
-                    <input type="text" className="w-full shadow-lg p-2 rounded-lg text-black outline-none" placeholder="Search any notes..."/>
+                    <input type="text" 
+                        className="w-full shadow-lg p-2 rounded-lg text-black outline-none" 
+                        placeholder="Search any notes..."
+                        onChange={(e)=>{
+                            handleOnSearch(e.target.value);
+                        }}
+                    />
+                    {suggestedNotesArray?.length>0 && <div className="relative">
+                        <div className="bg-white h-auto overflow-y-auto w-full text-black p-2 absolute z-50 top-2 rounded-lg cursor-pointer">
+                            {suggestedNotesArray?.slice(0, SIZE)?.map((note)=><div>{note?.title}</div>)}
+                        </div>
+                    </div>}
                 </div>
                 <div className="flex">
                     <div className="m-1">
